47ea2b04bc5933a9460f3ad096c4f665cc0059fb
JakeDyme <JakeDyme@gmail.com>
2018-06-25 20:04:54 +0200

    Added failing test

--- EasyRuleToDymeRules.Tests/EasyRuleToDymeRulesTests.cs
+++ EasyRuleToDymeRules.Tests/EasyRuleToDymeRulesTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using DymeRuleEngine.Constructs;
+using EasyRule.Dyme;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +12,18 @@ namespace EasyRuleToDymeRules.Tests
     [TestFixture]
     public class EasyRuleToDymeRulesTests
     {
+        [Test]
+        public void EasyRuleDymeParser_GivenEasyRules_ExpectDymeRules()
+        {
+            // Arrange...
+            var sut = new EasyRuleDymeParser();
+            var inputEasyRule = "IF (Age IS 18) THEN (Movie IS Visible)";
+            var expectDymeRule = Imply.Create().If(Fact.That("Age").Is("18")).Then(Fact.That("Movie").Is("Visible"));
+            // Act...
+            var result = sut.ConvertEasyRuleToDymeRule(inputEasyRule);
 
+            Assert.AreEqual(expectDymeRule, result);
+        }
         
 
     }
